1f12cd9248fb522411f5236eee0b2cacf6d97b3d
Screwy Uncle Louie <sulouie@Screwys-MacBook-Pro.local>
2023-09-28 19:41:57 -0400

    Make sure to credit Sports Dad for project.

---
+++ BloodRobotDoctor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BloodRobotDoctor
+//
+//  Created by Sports Dad on 9/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
