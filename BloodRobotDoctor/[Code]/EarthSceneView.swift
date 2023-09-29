5a9f5494e43aba36a94baa1a827d2893d0563d71
Screwy Uncle Louie <sulouie@Screwys-MacBook-Pro.local>
2023-09-29 02:42:08 -0400

    added lymphocyte and platelet image, test render

--- BloodRobotDoctor/[Code]/EarthSceneView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MetalViewControllerRepresentable.swift
-//  BloodRobotDoctor
-//
-//  Created by Sports Dad on 2/10/23.
-//
-
-import SwiftUI
-
--- BloodRobotDoctor/[Code]/MedicalScene.swift
+++ BloodRobotDoctor/[Code]/MedicalScene.swift
@@ -14,8 +14,12 @@ class MedicalScene: GraphicsDelegate {
     
     let recycler = RecyclerShapeQuad2D()
     
+    let recyclerSprite2D = RecyclerSprite2D()
+    
+    let sprite = Sprite2D()
+    
     func load() {
-        
+        sprite.load(graphics: graphics, fileName: "LY_9109.jpg")
     }
     
     @objc func receive(notification: Notification) {
@@ -26,7 +30,7 @@ class MedicalScene: GraphicsDelegate {
     
     func update() {
         
-        ROTATEEE += 0.1
+        ROTATEEE += 0.005
         if ROTATEEE >= Float.pi * 2.0 {
             ROTATEEE -= Float.pi * 2.0
         }
@@ -54,19 +58,22 @@ class MedicalScene: GraphicsDelegate {
     
     func draw2D(renderEncoder: MTLRenderCommandEncoder) {
         
+        recycler.reset()
+        recyclerSprite2D.reset()
+        
         
         var projection = matrix_float4x4()
         projection.ortho(width: graphics.width,
                          height: graphics.height)
         
         var modelView = matrix_identity_float4x4
-        modelView.translate(x: graphics.width * 0.5, y: graphics.height * 0.5, z: 0.0)
+        modelView.translate(x: graphics.width * 0.25, y: graphics.height * 0.5, z: 0.0)
         modelView.rotateZ(radians: Float(ROTATEEE))
         
         graphics.set(pipelineState: .shape2DAlphaBlending,
                      renderEncoder: renderEncoder)
         
-        recycler.reset()
+        
         
         recycler.set(red: 1.0, green: 0.0, blue: 0.25, alpha: 1.0)
         
@@ -79,6 +86,21 @@ class MedicalScene: GraphicsDelegate {
                            size: 120.0)
         
         
+        
+        
+        graphics.set(pipelineState: .sprite2DAlphaBlending,
+                     renderEncoder: renderEncoder)
+        
+        modelView = matrix_identity_float4x4
+        modelView.translate(x: graphics.width * 0.75, y: graphics.height * 0.5, z: 0.0)
+        modelView.rotateZ(radians: Float(ROTATEEE))
+        
+        recyclerSprite2D.set(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        recyclerSprite2D.draw(graphics: graphics,
+                              renderEncoder: renderEncoder,
+                              projection: projection,
+                              modelView: modelView,
+                              sprite: sprite)
         
         /*
         recyclerShapeQuad2D.reset()
